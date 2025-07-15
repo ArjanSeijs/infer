61e3b35bf0898096941e8260ca0aedf1bd7b39d4
Arjan Seijs <arjan.seijs@gmail.com>
2025-07-15 13:47:10 +0200

    Test setup

--- infer/lib/rust/src/main.rs
+++ infer/lib/rust/src/main.rs
@@ -1,52 +1,18 @@
 #![feature(rustc_private)]
-extern crate rustc_driver;
-extern crate rustc_hir;
-extern crate rustc_interface;
-extern crate rustc_middle;
-
-#[macro_use]
-extern crate rustc_smir;
-extern crate stable_mir;
-use std::{env, ops::ControlFlow};
-
-mod textual;
-#[allow(unused)]
-mod textual_defs;
-use stable_mir::CompilerError;
-use textual::mir_to_textual;
+use infer_rustc_mir::call_compiler;
+use std::env;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     compiler_arguments(&mut args);
-    call_compiler(args);
-}
-
-pub fn call_compiler(args: Vec<String>) {
-    let analyze_code = || -> ControlFlow<Result<String, String>, String> {
-        let result = std::panic::catch_unwind(|| {
-            mir_to_textual(stable_mir::all_local_items())
-        });
-        match result {
-            Ok(translation) => ControlFlow::Break(Ok(translation)),
-            Err(e) => ControlFlow::Break(Err(format!("{:?}", e))),
-        }
-    };
-    let result = run!(&args, analyze_code);
+    let result = call_compiler(&args);
     match result {
-        Ok(ok) => println!("{}", ok),
-        Err(CompilerError::Interrupted(Ok(s))) => println!("{}", s),
-        Err(CompilerError::Interrupted(Err(s))) => println!("// Error {}", s),
-        Err(err) => println!("// CompileError : {:?}", err),
+        Ok(s) => println!("{}", s),
+        Err(s) => println!("{}", s),
     }
 }
 
 fn compiler_arguments(args: &mut Vec<String>) {
-
-    let mut i = 0;
-    let mut ins = || {
-        i += 1;
-        i
-    };
     // Release Mode (No Alignment Checks)
     args.insert(1, "-C".to_string());
     args.insert(2, "opt-level=3".to_string());
@@ -57,8 +23,4 @@ fn compiler_arguments(args: &mut Vec<String>) {
     args.insert(6, "unused_variables".to_string());
     args.insert(7, "-A".to_string());
     args.insert(8, "unused_must_use".to_string());
-    
-    args.insert(ins(), "--crate-type=lib".to_string());
-    // args.insert(ins(), "-Z".to_string());
-    // args.insert(ins(), "unpretty=mir".to_string());
 }
--- infer/lib/rust/tests/test.rs
+++ infer/lib/rust/tests/test.rs
@@ -1,18 +1,42 @@
-use infer_rustc_mir::call_compiler; 
+#![feature(rustc_private)]
+use infer_rustc_mir::call_compiler;
 
 fn run_test(args: &mut Vec<String>, file_name: &str) {
+    // 'executable'
+    args.insert(0, "cargo".to_string());
     // Do no optimisation on test
     args.push("-Z".to_string());
     args.push("mir-opt-level=0".to_string());
+
     // Do not require main function
     args.push("--crate-type=lib".to_string());
+
+    // Release Mode (No Alignment Checks)
+    args.push("-C".to_string());
+    args.push("opt-level=3".to_string());
+    
+    // Allow unused code.
+    args.push("-A".to_string());
+    args.push("dead_code".to_string());
+    args.push("-A".to_string());
+    args.push("unused_variables".to_string());
+    args.push("-A".to_string());
+    args.push("unused_must_use".to_string());
+
     // Finally push filename
     args.push(file_name.to_string());
-    call_compiler(args);
+    call_compiler(&args);
 }
 
+#[allow(unused)]
 fn print_mir(args: &mut Vec<String>, file_name: &str) {
     args.push("-Z".to_string());
     args.push("unpretty=mir".to_string());
     run_test(args, file_name);
+}
+
+#[test]
+fn add0() {
+    run_test(&mut vec![], "./tests/example_files/add0.rs");
+    // print_mir(&mut vec![], "./tests/example_files/add0.rs");
 }
