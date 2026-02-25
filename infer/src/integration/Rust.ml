b615137c72beaf1084a679dce7764cec8c6f01d2
Arjan Seijs <arjan.seijs@gmail.com>
2026-02-25 20:53:58 +0100

    [rust] Cleaned up error logging

--- infer/src/integration/Rust.ml
+++ infer/src/integration/Rust.ml
@@ -46,7 +46,7 @@ let compile prog args =
   In_channel.close stderr ;
   match IUnix.waitpid pid with
   | Error _ as status ->
-      L.die ExternalError "Rustc exited with: %s@\n Trace: %s\n%s\n"
+      L.die ExternalError "Rustc exited with: %s @. Output: %s @. Trace: %s@."
         (IUnix.Exit_or_signal.to_string_hum status)
         output error
   | Ok () ->
@@ -70,7 +70,7 @@ let load_textual_models filenames =
           TextualParser.TextualFile.capture ~use_global_tenv:true sil ;
           Tenv.merge ~src:sil.tenv ~dst:acc_tenv
       | Error (sourcefile, errs) ->
-          List.iter errs ~f:(L.external_error "%a@\n" (TextualParser.pp_error sourcefile)) ) ;
+          List.iter errs ~f:(L.external_error "%a @." (TextualParser.pp_error sourcefile)) ) ;
   acc_tenv
 
 
@@ -95,7 +95,7 @@ let filename_from_json json =
   | Some file_name ->
       file_name
   | None ->
-      L.die UserError "No file found in crate"
+      L.die UserError "No file found in crate @."
 
 
 let capture_file json_filename =
@@ -106,7 +106,7 @@ let capture_file json_filename =
     | Ok crate ->
         RustFrontend.RustMir2Textual.mk_module crate ~file_name
     | Error err ->
-        L.die UserError "%s: %s" err (Yojson.Basic.to_string json)
+        L.die UserError "%s: %s @." err (Yojson.Basic.to_string json)
   in
   let sourcefile = Textual.SourceFile.create file_name in
   if Config.debug_mode || Config.dump_textual then dump_textual_file json_filename textual ;
@@ -115,7 +115,7 @@ let capture_file json_filename =
     | Ok vt ->
         vt
     | Error err ->
-        L.die UserError "Textual verification failed:%a"
+        L.die UserError "Textual verification failed: %a @."
           (F.pp_print_list TextualVerification.pp_error)
           err
   in
@@ -128,7 +128,7 @@ let capture_file json_filename =
     | Ok s ->
         s
     | Error err ->
-        L.die UserError "Module to sil failed: %a"
+        L.die UserError "Module to sil failed: %a @."
           (F.pp_print_list (Textual.pp_transform_error sourcefile))
           err
   in
@@ -140,8 +140,8 @@ let capture_file json_filename =
 
 
 let capture prog (args : string list) =
-  if not (String.equal prog "rustc") && not (String.equal prog "cargo") then
-    L.die UserError "rustc or cargo should be explicitly used instead of %s." prog ;
+  if (not (String.equal prog "rustc")) && not (String.equal prog "cargo") then
+    L.die UserError "rustc or cargo should be explicitly used instead of %s. @." prog ;
   let json_filename = compile prog args in
   capture_file json_filename
 
